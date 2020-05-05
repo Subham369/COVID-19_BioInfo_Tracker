f06cca8cf93924b24f93dc05823c1bd6a2ced16b
Subham369 <subhammohanta27@gmail.com>
2020-05-05 08:45:29 +0530

    Updated Feature

--- app/src/main/java/com/example/covid_19bioinfotracker/AppFeatures.java
+++ app/src/main/java/com/example/covid_19bioinfotracker/AppFeatures.java
@@ -1,9 +1,13 @@
 package com.example.covid_19bioinfotracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AppFeatures extends AppCompatActivity {
@@ -16,6 +20,31 @@ public class AppFeatures extends AppCompatActivity {
 
     public void itemWorkerLocation(View view) {
         Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void itemProfile(View view) {
+        Intent intent=new Intent(getApplicationContext(),UserProfile.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.item_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.help){
+            Intent intent=new Intent(getApplicationContext(),HelpLine.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clkFund(View view) {
+        Intent intent=new Intent(getApplicationContext(),FundActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/covid_19bioinfotracker/UserProfile.java
@@ -0,0 +1,58 @@
+package com.example.covid_19bioinfotracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+public class UserProfile extends AppCompatActivity {
+
+    private TextView name,dob,phone,email,address;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        name=findViewById(R.id.txtName);
+        dob=findViewById(R.id.txtDOB);
+        phone=findViewById(R.id.txtPhone);
+        email=findViewById(R.id.txtEmail);
+        address=findViewById(R.id.txtAddress);
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+
+        databaseReference=firebaseDatabase.getReference(Objects.requireNonNull(firebaseAuth.getUid()));
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user=dataSnapshot.getValue(User.class);
+                if (user == null)
+                    throw new AssertionError();
+                name.setText(user.getName());
+                dob.setText(user.getDob());
+                phone.setText(user.getPhone());
+                email.setText(user.getEmail());
+                address.setText(user.getAddress());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UserProfile.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
